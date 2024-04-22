d260abbba4fcf64b05df6aa6d7fd4e3f923206a2
Víctor Ballestrini <vsballestrini@gmail.com>
2024-04-22 20:13:18 -0300

    Create base project

--- src/RodaVelha/obj/Debug/net8.0/RodaVelha.AssemblyInfo.cs
+++ src/RodaVelha/obj/Debug/net8.0/RodaVelha.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RodaVelha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c53e96cbc58bc9bf7281fcf6a5770bc09c9b302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04be2c974ed7b74b0525d6ed70626471ea9b6153")]
 [assembly: System.Reflection.AssemblyProductAttribute("RodaVelha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RodaVelha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
