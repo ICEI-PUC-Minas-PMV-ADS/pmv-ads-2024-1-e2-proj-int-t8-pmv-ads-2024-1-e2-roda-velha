d16e368baeaa5643f6611dbbac904342708156f1
Arthur Simoes <arthursimoes155@gmail.com>
2024-05-12 16:06:34 -0300

    :sparkles: Add validations and error messages to login and register forms

--- src/RodaVelha/Models/User.cs
+++ src/RodaVelha/Models/User.cs
@@ -1,10 +1,20 @@
-﻿namespace RodaVelha.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RodaVelha.Models;
 
 public class User
 {
     public int ID { get; set; }
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    [Display(Name ="Nome")]
     public required string Name { get; set; }
+    [Required(ErrorMessage ="Email é obrigatório.")]
+    [EmailAddress(ErrorMessage = "Email inválido.")]
+    [Display(Name ="Email")]
     public required string Email { get; set; }
+    [Required(ErrorMessage = "A senha é obrigatória.")]
+    [DataType(DataType.Password)]
+    [Display(Name ="Senha")]
     public required string Password { get; set; }
     public string? Photo { get; set; }
 
