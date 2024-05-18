489efbd01d81b4c415718ab066262aa0f9644422
Arthur Simoes <arthursimoes155@gmail.com>
2024-05-18 17:05:41 -0300

    :sparkles: Create EditUserViewModel

---
+++ src/RodaVelha/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RodaVelha.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [Display(Name="Nome")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage ="Email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Email inválido.")]
+        [Display(Name="Email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Display(Name="Nova senha")]
+        public string? NewPassword { get; set; } = null;
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [Display(Name="Senha*")]
+        public string Password { get; set; } = string.Empty;
+
+        [Display(Name="Foto")]
+        public string? Photo { get; set; }
+    }
+}
