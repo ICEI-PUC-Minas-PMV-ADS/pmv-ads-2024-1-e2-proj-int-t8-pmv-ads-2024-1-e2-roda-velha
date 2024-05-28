0804664099a441cb7d5825ff229a8f1c56608cba
dinhoop <anderson@digitallone.net>
2024-05-27 21:12:59 -0600

    create: View Report Page

--- src/RodaVelha/Controllers/UsersController.cs
+++ src/RodaVelha/Controllers/UsersController.cs
@@ -150,6 +150,12 @@ namespace RodaVelha.Controllers
             return View();
         }
 
+        // GET: Users/Reports
+        public IActionResult Report()
+        {
+            return View();
+        }
+
         // POST: Users/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
