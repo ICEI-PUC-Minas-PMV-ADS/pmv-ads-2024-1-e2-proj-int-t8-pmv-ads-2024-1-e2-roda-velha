d9784bc3f8bd724313320483ab7fd77160bef021
dinhoop <anderson@digitallone.net>
2024-06-06 15:05:20 -0600

    create: like Javascript file functionality

--- src/RodaVelha/wwwroot/js/like.js
+++ src/RodaVelha/wwwroot/js/like.js
@@ -1,14 +1,18 @@
 ﻿document.addEventListener("DOMContentLoaded", function ()
 {
-    var likeButton = document.getElementById("likeButton");
-    likeButton.addEventListener("click", function () {
-        var eventId = this.getAttribute("data-event-id");
+    var likeButtons = document.querySelectorAll(".likeButton");
+    likeButtons.forEach(function(button){
 
+        var eventId = this.getAttribute('data-event-id');
+        var data = {
+            eventId: eventId
+        };
+        
         var req = new XMLHttpRequest();
         req.open("POST", "Like/LikeEvent", true);
-        req.setRequestHeader("Content-Type", "pplication/x-www-form-urlencoded");
+        req.setRequestHeader("Content-Type", "Application/x-www-form-urlencoded");
         req.onreadystatechange = function () {
-            if (req.readyState == 4) {
+            if (req.readyState == XMLHttpRequest.DONE) {
                 if (req.readyState == 200) {
                     var response = JSON.parse(req.responseText);
                     if (response.success)
@@ -21,6 +25,9 @@
                 }
             }
         }
-        req.send("eventId=" + encodeURIComponent(eventId));
+        req.send(JSON.stringify(data));
+
+
+
     });
 })
